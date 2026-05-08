700c301a44e8b664bfc55522d48750f5e68fb3a8
Mikaël Sévigny <sevignator@hotmail.com>
2026-05-08 14:15:58 -0400

    Prevent passwordHash from being passed in AuthContext

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -7,11 +7,14 @@ import {
   type ReactNode,
 } from 'react'
 import { useServerFn } from '@tanstack/react-start'
+
 import { type User } from '#/db/schema'
 import { getCurrentUserFn } from '#/utils/users.functions'
 
+type UserWithoutPassword = Omit<User, 'passwordHash'>
+
 interface AuthContextType {
-  user: User | null
+  user: UserWithoutPassword | null
   fetchUser: () => Promise<void>
 }
 
@@ -19,12 +22,16 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const getCurrentUser = useServerFn(getCurrentUserFn)
-  const [user, setUser] = useState<User | null>(null)
+  const [user, setUser] = useState<UserWithoutPassword | null>(null)
 
   const fetchUser = React.useCallback(async () => {
     try {
-      const nextUser = await getCurrentUser()
-      setUser(nextUser)
+      const currentUser = await getCurrentUser()
+
+      if (currentUser) {
+        const { passwordHash, ...nextUser } = currentUser
+        setUser(nextUser)
+      }
     } catch (error) {
       console.error('Failed to fetch current user', error)
       setUser(null)
