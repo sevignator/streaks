68738096429ed223fd712bbaf978055a04640d72
Mikaël Sévigny <sevignator@hotmail.com>
2026-05-12 10:01:27 -0400

    Remove unused property

--- src/utils/email.functions.ts
+++ src/utils/email.functions.ts
@@ -10,7 +10,7 @@ const resend = new Resend(env.RESEND_API_KEY);
 export const sendEmailFn = createServerFn({ method: 'POST' })
   .inputValidator((input: z.input<typeof emailSchema>) => input)
   .handler(async ({ data: handlerData }) => {
-    const { data, error } = await resend.emails.send({
+    const { error } = await resend.emails.send({
       from: 'Streaks <notification@streaks.fyi>',
       to: handlerData.to,
       subject: handlerData.subject,
